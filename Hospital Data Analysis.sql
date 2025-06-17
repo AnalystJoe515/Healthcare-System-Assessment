595510a73c225c4dc8d4ec15aa56f9267ca77501
Joshua Matthew <sleekdigitalconcept@gmail.com>
2025-06-17 17:36:21 +0100

    Hospital Data Analysis.sql

--- Hospital Data Analysis.sql
+++ Hospital Data Analysis.sql
@@ -87,7 +87,6 @@ HAVING count(*) >1;
 /* 1. Investigate the lab dataset to examine patients’ blood sugar levels. 
 Typically, fasting Blood sugar levels fall between 70 and 100 mg/dL. The goal is to identify patients whose lab results are outside this normal range to implement early interventions */
 
-CREATE VIEW Abnormal_FBSL AS
 SELECT p.patient_id, p.patient_name, lr.result_value
 FROM public."Patients_data_table" AS p
 INNER JOIN public."Outpatient_visit" AS ov
@@ -98,7 +97,7 @@ WHERE lr.test_name = 'Fasting Blood Sugar'
 AND (CAST(lr.result_value AS NUMERIC) < 70 OR CAST(lr.result_value AS NUMERIC) > 100);
 
 -- 2. The Hospital management wants to prevent cardiovascular disease and they need to assess how many patients are considered High, Medium, and Low Risk based on hypertension and diabetes diagnosis, smoker status
-CREATE VIEW CVD_risk_assessment AS
+
 SELECT 
 CASE 
     WHEN Smoker_status = 'Y' AND Diagnosis IN ('Hypertension', 'Diabetes') THEN 'HIGH RISK'
@@ -115,7 +114,7 @@ CASE
 END;
 
 --3 Identify individuals at high risk of developing obesity within a population based on bmi and hyperlipidemia dignosis
-CREATE VIEW Obesity_risk AS 
+
 SELECT p.patient_id, p.patient_name,
 CASE 
     WHEN diagnosis = 'Hyperlipidemia' AND bmi > 30 THEN 'HIGH RISK'
@@ -130,7 +129,7 @@ ON ov.patient_id = hr.patient_id;
 
 
 --4 Flag patients who are at risk due to interaction between their medication and smoking status
-CREATE VIEW Medic_and_smoking_concern AS 
+
 SELECT
 patient_id,
 diagnosis,
@@ -144,7 +143,7 @@ END AS safety_concern
 FROM public."Outpatient_visit";
 
 --5 Classify patients into high, medium or low risk based on their BMI and family risk of hypertension
-CREATE VIEW Hypertension_risk AS
+
 
 SELECT 
 CASE 
@@ -164,7 +163,7 @@ END;
 --Population Demographics
 
 -- 1. What is the distribution of patients in our healthcare system based on their age groups and genders
-CREATE VIEW Patient_distribution AS 
+
 SELECT Gender,
 CASE
     WHEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, DATE_OF_BIRTH)) <= 17 THEN 'Pediatric'
@@ -182,7 +181,7 @@ END
 ORDER BY gender DESC;
 
 --2. How many patients have been admitted in each department so far?
-CREATE VIEW Dept_admission AS 
+ 
 SELECT department_name,
 COUNT(*) AS Patient_count
 FROM public."Hospital_data_records"
@@ -190,7 +189,6 @@ GROUP BY department_name
 ORDER BY Patient_count DESC;
 
 --3. Detailed information of patients who have visited the hospital
-CREATE VIEW Patient_info AS
 
 SELECT p.Patient_name, ov.doctor_name, ov.visit_date, ov.reason_for_visit, ov.diagnosis, 
 lr.test_name, lr.result_value,  ov.medication_prescribed, hr.days_in_the_hospital
@@ -201,7 +199,7 @@ INNER JOIN public."Lab_results" as lr ON ov.visit_id = lr.visit_id
 ORDER BY patient_name ASC;
 
 --4. How does the frequency of fasting blood sugar testing vary between gender and age group?
-CREATE VIEW FBSL_freq AS 
+
 SELECT Gender,
 CASE
     WHEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, DATE_OF_BIRTH)) <= 17 THEN 'Pediatric'
@@ -222,7 +220,7 @@ END
 ORDER BY test_count;
 
 -- 5. Which department should receive additional resources (e.g., staff, equipment) to manage a high volume of test screenings
-CREATE VIEW Dept_resource AS 
+
 SELECT hr.department_name,
 COUNT(test_name) AS test_count
 FROM public."Hospital_data_records" as hr
@@ -234,7 +232,7 @@ ORDER BY test_count DESC;
 --Clinical research (Present diseases and demographic characteristics of diseases)
 
 --1. Which diagnoses are most prevalent among patients, and how do they vary across the different demographic groups, including gender and age
-CREATE VIEW Demo_Freq_diagnosis AS 
+
 SELECT p.Gender, ov.diagnosis,
 CASE
     WHEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, p.DATE_OF_BIRTH)) <= 17 THEN 'Pediatric'
@@ -254,14 +252,14 @@ END
 ORDER BY gender DESC;
 
 --2. What are the most commonly ordered lab tests
-CREATE VIEW Freq_labtest AS
+
 SELECT test_name, COUNT(test_name) AS test_count
 FROM public."Lab_results"
 GROUP BY test_name
 ORDER BY test_count DESC;
 
 --3. Are there significant differences in diagnosis between smokers and non-smokers?
-CREATE VIEW SMOKING_diagnosis AS 
+
 SELECT Smoker_status,
 COUNT(diagnosis) AS Diagnosis_count
 FROM public."Outpatient_visit"
@@ -269,14 +267,14 @@ GROUP BY Smoker_status
 ORDER BY Diagnosis_count DESC;
 
 --4. Identify a cohort of patients with chronic diseases, including hypertension, hyperlipidemia, and diabetes.
-CREATE VIEW Chronic_disease AS 
+
 SELECT p.patient_id, p.patient_name, ov.diagnosis
 FROM public."Patients_data_table" as p
 INNER JOIN public."Outpatient_visit" as ov ON p.patient_id = ov.patient_id
 WHERE diagnosis IN ('Hypertension', 'Hyperlipidemia', 'Diabetes'); 
 
 --5. Investigate the most frequent reasons for patients visit to the hospital
-CREATE VIEW Freq_visit AS
+
 SELECT reason_for_visit,
 COUNT(*) AS Reason_count
 FROM public."Outpatient_visit"
@@ -286,14 +284,14 @@ ORDER BY Reason_count DESC;
 --Healthcare Admin (reason for visit; appointment date and reminders)
 
 --1. What are the most common appointment times throughout the day, and how does the distribution of appointment times vary across different hours?
-CREATE VIEW Appointment_record AS
+
 SELECT DATE_PART('hour', Appointment_time) AS Hour, COUNT(*) AS Appointment_count
 FROM public."Appointment_data_analysis"
 GROUP BY DATE_PART('hour', Appointment_time)
 ORDER BY Appointment_count DESC;
 
 --2. The hospital administration is interested in finding out information about the patients who had multiple visits within 30 days of their previous medical visit. Write a query to identify those patients, the date of the initial visit, the reason for the initial visit, the readmission date, the reason for readmission, and the number of days between the initial visit and readmission.
-CREATE VIEW Patient_visit AS
+
 SELECT ov_initial.patient_id, 
        ov_initial.visit_date AS initial_visit, 
        ov_readmit.visit_date AS readmit_visit, 
@@ -307,7 +305,7 @@ WHERE ov_readmit.visit_date > ov_initial.visit_date
 AND ov_readmit.visit_date <= ov_initial.visit_date + INTERVAL '30 days';
 
 --3. Compare the average number of days the patients are spending in each department of the hospital
-CREATE VIEW avg_days_hosp AS
+
 SELECT department_name,
 AVG(days_in_the_hospital) AS Average_num_days
 FROM public."Hospital_data_records"
